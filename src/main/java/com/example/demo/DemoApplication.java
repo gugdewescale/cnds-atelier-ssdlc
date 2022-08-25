7c053156df5308cbf9d511d90b64e9f731050b54
Guillaume Guérard <guillaum.guerard@wescale.fr>
2022-08-26 00:33:08 +0200

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	private static final String MY_SECRET = "47828a8dd77ee1eb9dde2d5e93cb221ce8c32b37";
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
